7afda6b8645cb6b8e84a9dff7268e2542138bb2f
Aurelio Malheiros <aureliomalheiros@gmail.com>
2023-12-04 22:01:41 -0300

    modify files

--- k8s/scripts/control-plane.sh
+++ k8s/scripts/control-plane.sh
@@ -1,4 +1,61 @@
 #!/bin/bash
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+# sysctl
+sudo sysctl --system
+
+# Install requirements
+
+sudo apt update && \
+sudo apt install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release -y
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
+	sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
+| sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update SO
+sudo apt-get update
+
+# Repository Docker
+
+sudo apt update && sudo apt install -y containerd.io -y
+
+sudo mkdir -p /etc/containerd && containerd config default | sudo tee /etc/containerd/config.toml 
+
+sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
+
+sudo systemctl restart containerd
+
+# Install kubeadm, kubectl and kubelete
+
+sudo apt-get update && \
+sudo apt-get install -y apt-transport-https ca-certificates curl
+curl -fsSL https://dl.k8s.io/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update && \
+sudo apt-get install -y kubelet kubeadm kubectl 
+sudo apt-mark hold kubelet kubeadm kubectl 
+
 kubeadm init --apiserver-advertise-address=0.0.0.0
 
 mkdir -p $HOME/.kube
